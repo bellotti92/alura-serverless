d74dfe63eccc4161a7176cbcc0271d6eafd3b201
Bellotti <bellotti92>
2021-10-23 16:00:12 -0300

    aula 03

--- handler.js
+++ handler.js
@@ -7,12 +7,41 @@ const pacientes = [
 ];
 
 module.exports.listarPacientes = async (event) => {
-  console.log(event);
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         pacientes,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.obterPaciente = async (event) => {
+  const { pacienteId } = event.pathParameters;
+
+  const paciente = pacientes.find((paciente) => paciente.id == pacienteId);
+
+  if (paciente === undefined) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify(
+        {
+          error: "Paciente não existe!",
+        },
+        null,
+        2
+      ),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        paciente,
       },
       null,
       2
